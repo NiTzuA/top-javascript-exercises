bbd48dbc7011c5cc51439a30c15206937fef38cc
NiTzuA <11v35faustin@gmail.com>
2024-11-19 17:32:43 +0800

    Completed stage 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+        return "ERROR";
+    }
+    if (max < min) {
+        temp = max;
+        max = min;
+        min = temp;
+    }
+    let output = 0;
+    for (let i = min; i <= max; i++)
+    {
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
