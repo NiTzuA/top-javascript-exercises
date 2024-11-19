427c1c5e825ba7d94b5983c916a074d2a208a658
NiTzuA <11v35faustin@gmail.com>
2024-11-19 17:20:58 +0800

    Completed stage 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+    const numArr = arguments[0];
+    let removedNums = new Map();
+    let output = [];
+
+    for (let i = 1; arguments[i] != null; i++)
+    {
+        removedNums.set(arguments[i], i);
+    }
+
+    for (let i = 0; i < numArr.length; i++) {
+        if (!removedNums.has(numArr[i])) {
+            output.push(numArr[i]);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
