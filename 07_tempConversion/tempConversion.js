6d1adf4f60e25499d090d0a14dc5bf13cd7597c6
NiTzuA <11v35faustin@gmail.com>
2024-11-26 08:58:08 +0800

    Finished stage 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  // The Formula for converting F to C is F - 32 / 1.8
+  let celsius = (farenheit - 32) / 1.8;
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // The Formula for converting C to F is C * 1.8 + 32
+  let farenheit = (celsius * 1.8) + 32;
+  return Number(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
