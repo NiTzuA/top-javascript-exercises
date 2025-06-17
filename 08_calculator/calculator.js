15e6447b704807af9e7ad04d2709c42b59356a44
NiTzuA <11v35faustin@gmail.com>
2025-06-17 11:15:52 +0800

    Finished Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, arr) => sum + arr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, arr) => prod * arr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) { return 1; } 
+  let prod = 1;
+  while (num > 0) {
+    prod = prod * num;
+    num--;
+  }
+  return prod;
 };
 
 // Do not edit below this line
