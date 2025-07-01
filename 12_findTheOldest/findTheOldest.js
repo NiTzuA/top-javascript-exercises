65b22c87295839f610ca2b7aac256f1909ea0881
NiTzuA <11v35faustin@gmail.com>
2025-07-01 10:06:03 +0800

    Finished findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldestPerson = people.reduce((oldest, current) => {
+        const currentYear = new Date().getFullYear();
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
