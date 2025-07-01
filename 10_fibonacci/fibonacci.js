a6947c8548f1a32309f066b345083bf91e68c9c9
NiTzuA <11v35faustin@gmail.com>
2025-07-01 09:28:39 +0800

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fiboSeq = [0, 1];
+    while (num - 2 >= 0) {
+        let nextVal = fiboSeq[0] + fiboSeq[1];
+        fiboSeq.shift();
+        fiboSeq.push(nextVal);
+        num--;
+    }
+    return num == 0 ? 0 : fiboSeq[1];
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
